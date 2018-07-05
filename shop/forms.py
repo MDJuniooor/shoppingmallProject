c99baeaf3ed338bdac3fe184e804d0acf82924de
DaeSung <anseotjd12@gmail.com>
2018-07-05 22:42:13 +0900

    최종 결제 모듈 완성

--- shop/forms.py
+++ shop/forms.py
@@ -5,6 +5,22 @@ from django.conf import settings
 from django.utils.safestring import mark_safe
 from django.utils.encoding import smart_text
 from django.template.loader import render_to_string
+from django import forms
+from .models import order
+from .mixins import IamportBaseForm
+
+
+class PayForm(IamportBaseForm):
+    template_name = 'shop/_iamport.html'
+    params_names = ['merchant_uid', 'name', 'amount']
+    imp_fn_name = 'request_pay'
+        
+    class Meta:
+        model = order
+        fields = ['imp_uid']
+        widgets = {
+            'imp_uid': forms.HiddenInput,
+        }
 
 class OrderForm(forms.ModelForm):
     class Meta:
@@ -14,7 +30,7 @@ class OrderForm(forms.ModelForm):
             'name': forms.TextInput(attrs={'readonly':'readonly'}),
             'amount': forms.TextInput(attrs={'readonly':'readonly'})
         }
-
+"""
 class PayForm(forms.ModelForm):
     class Meta:
         model =order
@@ -42,3 +58,4 @@ class PayForm(forms.ModelForm):
         oorder.save()
         return oorder
 
+"""
